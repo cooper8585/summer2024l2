05f3e7cf946123db8d06241976370db506fe70c3
cooper8585 <pawel.bednarz85@gmail.com>
2024-06-15 10:37:52 +0000

    second commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include "mbed.h"
 
+/*
+next term is the sum of the previous two terms.
+The first two terms of the Fibonacci sequence are 0 followed by 1.
+*/
 // main() runs in its own thread in the OS
+
+int t_1 = 0;
+int t_2 = 1;
+int next_term = 0;
+
+
+int max_terms = 10;
+
 int main()
 {
+
+    printif("Fibonacci series: %d,%d,", t_1, t_2);
+    for( int i=10; i>5 ; i--){
+        printif("loop %d \r\n", i);
+
+    }
+
+
     while (true) {
 
     }
